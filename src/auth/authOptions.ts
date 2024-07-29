a3f3665f087939f9fa09badfface57cff090887f
hazem <53303934+hazem-a1@users.noreply.github.com>
2024-07-29 15:16:46 +0300

    add google provider

--- src/auth/authOptions.ts
+++ src/auth/authOptions.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions, AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import dbConnect from '@/db/dbconnect';
 import clientPromise from '@/db/mongodb';
@@ -13,6 +14,17 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    })
   ],
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
